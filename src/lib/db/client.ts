f55561921f97f3eb23eab784841676fc9ec181ae
servicosia <servicosia@users.noreply.github.com>
2026-09-20 16:22:49 -0300

    corrigido

--- src/lib/db/client.ts
+++ src/lib/db/client.ts
@@ -4,9 +4,12 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
+const neonDbUrl = process.env.DATABASE_URL || "postgresql://neondb_owner:npg_jz4DyXNBUF3V@ep-snowy-salad-ac3a3a73-pooler.sa-east-1.aws.neon.tech/neondb?sslmode=require";
+
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
+    datasourceUrl: neonDbUrl,
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
   });
 
